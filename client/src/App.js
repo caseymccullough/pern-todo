9ae11aa285b56c3cbdd96382bd80929882ade8f2
McCullough <cmccullough@bcp.org>
2025-02-25 20:12:36 -0800

    adding more to client.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import { Fragment } from 'react';
 import './App.css';
 
+// components
+
+import InputTodo from './components/InputTodo';
+
 function App() {
-  return <Fragment> 
-    <h1>Test</h1>
-     </Fragment>;
+  return <Fragment>
+    <div className='container'>
+      <InputTodo />
+    </div>
+
+  </Fragment>;
 }
 
 export default App;

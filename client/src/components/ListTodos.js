ca69a1aa8a63dc24f8e3c03c975ced63d7106f00
McCullough <cmccullough@bcp.org>
2025-02-27 08:57:38 -0800

    finished the Delete Todo button

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -17,6 +17,25 @@ const ListTodos = () => {
         }
     }
 
+    // delete todo function
+
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+
+            // console.log(deleteTodo)
+
+            // necessary so the new item is removed automatically
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+
+        } catch (err) {
+            console.error(err.message)
+            
+        }
+    }
+
 
     useEffect(() => {
         getTodos();
@@ -44,7 +63,7 @@ const ListTodos = () => {
                     <tr key={todo.todo_id}>
                         <td>{todo.description}</td>
                         <td>Edit</td>
-                        <td>Delete</td>
+                        <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                     </tr>
                 ))
 
